79900bbdbf3eb702bb7ba20f53db1bb737d888af
Ali Green <hello@thenortherncoder.co.uk>
2026-04-19 00:07:21 +0100

    Adding accordion to split content

--- src/split-content/edit.js
+++ src/split-content/edit.js
@@ -38,6 +38,7 @@ export default function Edit({ attributes, setAttributes }) {
 							'core/table',
 							'core/buttons',
 							'core/button',
+							'tnc/accordion',
 						]}
 						templateLock={false}
 					/>

2dd5f70b0d8490263ecdf3b512f0954c629b0851
Ali Green <hello@thenortherncoder.co.uk>
2026-04-22 13:07:44 +0100

    Profile cards item

---
+++ src/profile-cards-item/edit.js
@@ -0,0 +1,120 @@
+import { __ } from '@wordpress/i18n';
+import { useState } from '@wordpress/element';
+import { useBlockProps, RichText, MediaPlaceholder, BlockControls, __experimentalLinkControl as LinkControl, } from '@wordpress/block-editor';
+import { Button, ToolbarGroup, ToolbarButton, Popover } from '@wordpress/components';
+import { Icon, trash, link } from '@wordpress/icons';
+import './editor.css';
+
+export default function Edit({ attributes, setAttributes }) {
+
+	const { name, address, telephone, email, buttonLabel, buttonUrl, imageID, imageURL } = attributes;
+	const [ isLinkPickerOpen, setIsLinkPickerOpen ] = useState( false );
+
+	const onSelectImage = ( media ) => {
+		setAttributes( {
+			imageID: media?.id,
+			imageURL: media?.url,
+		} );
+	};
+
+	const onRemoveImage = () => {
+		setAttributes( {
+			imageID: null,
+			imageURL: null
+		} );
+	};
+
+	return (
+		<div { ...useBlockProps({ className: 'tnc-block tnc-profile-cards-item tnc-block-content' }) }>
+
+			{ imageURL ? (
+				<>
+					<img
+						src={ imageURL }
+						alt={ __( 'image', 'tnc' ) }
+						className="object-cover rounded-2xl object-center aspect-6/4 mb-4"
+					/>
+					<Button
+						className='absolute top-4 left-4 !bg-white rounded cursor-pointer z-10 !p-1 !min-w-0 !h-auto'
+						onClick={ onRemoveImage }
+						label={ __( 'Remove image', 'tnc' ) }
+					>
+						<Icon size={20} icon={ trash } />
+					</Button>
+				</>
+			) : (
+				<MediaPlaceholder
+					onSelect = { onSelectImage }
+					allowedTypes = { [ 'image' ] }
+					multiples = { false }
+					labels = { { title: __( 'Select an image', 'tnc-blocks' ) } }
+				/>
+			)}
+
+			<RichText
+				tagName='h2'
+				withoutInteractiveFormatting={true}
+				allowedFormats={[]}
+				value={name}
+				onChange={(name) => setAttributes({ name: name })}
+				placeholder='Enter name...'
+				className='mb-0'
+			/>
+
+			<RichText
+				tagName='p'
+				withoutInteractiveFormatting={true}
+				allowedFormats={[]}
+				value={address}
+				onChange={(address) => setAttributes({ address: address })}
+				placeholder='Enter address...'
+				className='mb-0 mt-4!'
+			/>
+
+			<p className="mb-0 mt-2!">
+				<RichText
+					tagName='a'
+					withoutInteractiveFormatting={true}
+					allowedFormats={[]}
+					value={telephone}
+					onChange={(telephone) => setAttributes({ telephone: telephone })}
+					placeholder='Enter telephone...'
+				/>
+			</p>
+
+			<BlockControls>
+                <ToolbarGroup>
+                    <ToolbarButton
+                        icon={ link }
+                        label={ __( 'Set button link', 'tnc-blocks' ) }
+                        onClick={ () => setIsLinkPickerOpen( (open) => !open ) }
+                        isPressed={ isLinkPickerOpen }
+                    />
+                </ToolbarGroup>
+            </BlockControls>
+
+			{ isLinkPickerOpen && (
+                <Popover position="bottom center" onClose={ () => setIsLinkPickerOpen( false ) }>
+                    <LinkControl
+                        value={ { url: buttonUrl || '' } }
+                        onChange={ ( next ) => setAttributes( { buttonUrl: next?.url || '' } ) }
+                    />
+                </Popover>
+            ) }
+
+			<div className="wp-block-button mt-4">
+				<RichText
+					tagName='a'
+					withoutInteractiveFormatting={true}
+					allowedFormats={[]}
+					value={buttonLabel}
+					onChange={(buttonLabel) => setAttributes({ buttonLabel: buttonLabel })}
+					onClick={ ( e ) => e.preventDefault() }
+					placeholder='Enter button label...'
+					className='wp-block-button__link wp-element-button'
+				/>
+			</div>
+
+		</div>
+	);
+}
---
+++ src/profile-cards-item/render.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * PHP file to use when rendering the block type on the server to show on the front end.
+ *
+ * The following variables are exposed to the file:
+ *     $attributes (array): The block attributes.
+ *     $content (string): The block default content.
+ *     $block (WP_Block): The block instance.
+ *
+ * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
+ */
+
+$image_id = isset( $attributes['imageID'] ) ? (int) $attributes['imageID'] : 0;
+?>
+
+<div <?php echo get_block_wrapper_attributes([ 'class' => 'tnc-block tnc-profile-cards-item tnc-block-content' ]); ?>>
+	<?php
+		if ( isset( $image_id ) && $image_id ) :
+			echo wp_get_attachment_image(
+				$image_id,
+				'card-small',
+				false,
+				[
+					'class' => 'object-cover rounded-2xl object-center aspect-6/4 mb-4',
+					'alt'   => '',
+				]
+			);
+		else :
+			?>
+			<div class="w-full bg-gray-200 rounded-2xl aspect-6/4 mb-4"></div>
+			<?php
+		endif;
+
+		if ( isset( $attributes['name'] ) && $attributes['name'] ) :
+			?>
+			<h2 class="mb-0"><?php echo esc_html( $attributes['name'] ); ?></h2>
+			<?php
+		endif;
+
+		if ( isset( $attributes['address'] ) && $attributes['address'] ) :
+			?>
+			<p class="mb-0 mt-4!"><?php echo esc_html( $attributes['address'] ); ?></p>
+			<?php
+		endif;
+
+		if ( isset( $attributes['telephone'] ) && $attributes['telephone'] ) :
+			?>
+			<p class="mb-0 mt-2!"><a href="tel:<?php echo esc_html( $attributes['telephone'] ); ?>"><?php echo esc_html( $attributes['telephone'] ); ?></a></p>
+			<?php
+		endif;
+
+		if ( isset( $attributes['buttonLabel'] ) && $attributes['buttonLabel'] && isset( $attributes['buttonUrl'] ) && $attributes['buttonUrl'] ) :
+			$button_url = $attributes['buttonUrl'];
+			if ( strpos( $button_url, 'mailto:' ) !== 0 ) {
+				$button_url = 'mailto:' . ltrim( preg_replace( '#^https?://#', '', $button_url ) );
+			}
+			?>
+			<div class="wp-block-button mt-4">
+				<a href="<?php echo esc_url( $button_url ); ?>" class="wp-block-button__link wp-element-button inline-flex"><?php echo esc_html( $attributes['buttonLabel'] ); ?></a>
+			</div>
+			<?php
+		endif;
+	?>
+</div>

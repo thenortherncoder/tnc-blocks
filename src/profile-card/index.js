32d77e43c12fc6a737786ba305deeb48f8791869
Ali Green <hello@thenortherncoder.co.uk>
2026-04-22 13:07:29 +0100

    Profile card - replace with profile cards item

--- src/profile-card/index.js
+++
@@ -1,8 +0,0 @@
-import { registerBlockType } from '@wordpress/blocks';
-import './style.css';
-import Edit from './edit';
-import metadata from './block.json';
-
-registerBlockType( metadata.name, {
-	edit: Edit,
-} );

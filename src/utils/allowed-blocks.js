c3cad7f20d00e08145f19ba44476a0a649bb6f69
Ali Green <hello@thenortherncoder.co.uk>
2026-04-22 13:09:01 +0100

    Global allowed blocks

---
+++ src/utils/allowed-blocks.js
@@ -0,0 +1,12 @@
+export const ALLOWED_BLOCKS = [
+	'core/heading',
+	'core/paragraph',
+	'core/list',
+	'core/list-item',
+	'core/image',
+	'core/media',
+	'core/embed',
+	'core/table',
+	'core/buttons',
+	'core/button',
+]

b451820cf7cf7688755f43155d587412f19f1f67
Ali Green <hello@thenortherncoder.co.uk>
2026-04-22 13:08:27 +0100

    Content allowed blocks

--- src/content/edit.js
+++ src/content/edit.js
@@ -1,25 +1,28 @@
 import { __ } from '@wordpress/i18n';
 import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
+import { ALLOWED_BLOCKS } from '../utils/allowed-blocks';
 import './editor.css';
 
 export default function Edit() {
+
+	const TEMPLATE = [
+        ['core/paragraph', { placeholder: 'Start typing your content...' }],
+    ];
+
+	const ALLOWED_BLOCKS_TNC = [
+		'tnc/accordion',
+		'tnc/weather'
+	]
+
+	ALLOWED_BLOCKS.push(...ALLOWED_BLOCKS_TNC);
+
 	return (
 		<div { ...useBlockProps({ className: 'tnc-block tnc-content w-full max-w-full pl-gutter pr-gutter' }) }>
 			<div className="tnc-block-inner tnc-block-content max-w-wp-content ml-auto mr-auto">
 				<InnerBlocks
-					allowedBlocks={[
-						'core/heading',
-						'core/paragraph',
-						'core/list',
-						'core/list-item',
-						'core/image',
-						'core/embed',
-						'core/shortcode',
-						'core/table',
-						'core/buttons',
-						'core/button',
-					]}
+					allowedBlocks={ALLOWED_BLOCKS}
 					templateLock={false}
+					template={TEMPLATE}
 					renderAppender={InnerBlocks.ButtonBlockAppender}
 				/>
 			</div>

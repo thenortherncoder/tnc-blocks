e8bec335f4a733a9c7e3445f89c06a159e9e0b10
Ali Green <hello@thenortherncoder.co.uk>
2026-04-22 13:07:05 +0100

    Split content allowed blocks

--- src/split-content/edit.js
+++ src/split-content/edit.js
@@ -1,5 +1,6 @@
 import { __ } from '@wordpress/i18n';
 import { useBlockProps, InnerBlocks, RichText } from '@wordpress/block-editor';
+import { ALLOWED_BLOCKS } from '../utils/allowed-blocks';
 import './editor.css';
 
 export default function Edit({ attributes, setAttributes }) {
@@ -13,7 +14,6 @@ export default function Edit({ attributes, setAttributes }) {
 	return (
 		<div { ...useBlockProps({ className: 'tnc-block tnc-split-content w-full max-w-full pl-gutter pr-gutter' }) }>
 			<div className="tnc-block-inner max-w-wp-wide ml-auto mr-auto grid sm:grid-cols-[1fr_2fr] gap-8 lg:gap-12">
-
 				<RichText
 					tagName='h2'
 					withoutInteractiveFormatting={true}
@@ -23,24 +23,12 @@ export default function Edit({ attributes, setAttributes }) {
 					placeholder='Enter title...'
 					className='mb-0'
 				/>
-
 				<div className="tnc-block-content">
 					<InnerBlocks
-						template={TEMPLATE}
-						allowedBlocks={[
-							'core/heading',
-							'core/paragraph',
-							'core/list',
-							'core/list-item',
-							'core/image',
-							'core/embed',
-							'core/shortcode',
-							'core/table',
-							'core/buttons',
-							'core/button',
-							'tnc/accordion',
-						]}
+						allowedBlocks={ALLOWED_BLOCKS}
 						templateLock={false}
+						template={TEMPLATE}
+						renderAppender={InnerBlocks.ButtonBlockAppender}
 					/>
 				</div>
 			</div>

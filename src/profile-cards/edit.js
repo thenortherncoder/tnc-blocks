68039c39b13df99db8d2b7f34014d2059f8c3cff
Ali Green <hello@thenortherncoder.co.uk>
2026-04-23 12:08:50 +0100

    Profile cards using innerBlocksProps instead of wordpress default blocks for styling

--- src/profile-cards/edit.js
+++ src/profile-cards/edit.js
@@ -1,23 +1,32 @@
 import { __ } from '@wordpress/i18n';
-import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
+import { useBlockProps, useInnerBlocksProps, InnerBlocks } from '@wordpress/block-editor';
 import './editor.css';
 
 export default function Edit() {
+
+	const innerBlocksProps = useInnerBlocksProps(
+		{
+			className: 'grid sm:grid-cols-2 lg:grid-cols-3 gap-8'
+		},
+		{
+			allowedBlocks: ['tnc/profile-cards-item'],
+			templateLock: false,
+			template: [
+				['tnc/profile-cards-item'],
+						['tnc/profile-cards-item'],
+						['tnc/profile-cards-item']
+			],
+			orientation: "horizontal",
+			renderAppender: InnerBlocks.ButtonBlockAppender
+		}
+	);
+
 	return (
 		<div { ...useBlockProps({ className: 'tnc-block tnc-profile-cards w-full max-w-full pl-gutter pr-gutter' }) }>
 			<div className="tnc-block-inner max-w-wp-wide ml-auto mr-auto">
-				<InnerBlocks
-					allowedBlocks={[
-						'tnc/profile-cards-item'
-					]}
-					templateLock={false}
-					template={[
-						['tnc/profile-cards-item'],
-						['tnc/profile-cards-item'],
-						['tnc/profile-cards-item']
-					]}
-					orientation="horizontal"
-				/>
+
+				<div { ...innerBlocksProps } />
+
 			</div>
 		</div>
 	);

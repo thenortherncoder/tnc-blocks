b124001e2144448b603611309eecb81c5df481cd
Ali Green <hello@thenortherncoder.co.uk>
2026-04-23 12:06:31 +0100

    Removing the standard code from view.js

--- src/content/view.js
+++
@@ -1,4 +0,0 @@
-
-/* eslint-disable no-console */
-console.log("Hello World! (from tnc-content block)");
-/* eslint-enable no-console */
--- src/profile-cards/view.js
+++
@@ -1,3 +0,0 @@
-/* eslint-disable no-console */
-console.log("Hello World! (from tnc-profile-cards block)");
-/* eslint-enable no-console */
